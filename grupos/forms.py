1f4b0a2d677b2299e729ef22f597bcd30a7d82df
Miguel Angel <miguel.ojeda@konecta.com.py>
2015-10-16 15:19:54 -0400

    Asignar Usuarios  a grupo de proyecto

--- grupos/forms.py
+++ grupos/forms.py
@@ -1,26 +1,26 @@
 from django import forms
 from django.forms import ModelMultipleChoiceField
-from models import Rol
+from models import Grupo
 from django.contrib.auth.models import User
 
 
 
-class RolForm(forms.ModelForm):
+class GrupoForm(forms.ModelForm):
     """
-    Clase que contiene los campos del formulario, necesarios para el registro de nuevos User Story
+    Clase que contiene los campos del formulario, necesarios para el registro de nuevos Grupos
     en la base de datos.
     """
-    usuario = ModelMultipleChoiceField(User.objects.all(),
+    usuarios = ModelMultipleChoiceField(User.objects.all(),
             widget=forms.CheckboxSelectMultiple, required=True)
     class Meta:
-        model = Rol
-        fields = ['rol', 'usuario']
+        model = Grupo
+        fields = ['nombre', 'usuarios']
 
     def save(self, commit=True):
         if not commit:
-            raise NotImplementedError("Can't create Rol without database save")
-        rol = super(RolForm, self).save(commit=True)
+            raise NotImplementedError("Can't create grupo without database save")
+        grupo = super(GrupoForm, self).save(commit=True)
 
-        return rol
+        return grupo
 
 
