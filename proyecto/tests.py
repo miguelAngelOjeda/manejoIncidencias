1e8d7408efff23e6c20f14e3c4472fb6690e69dd
unknown <mepm19@gmail.com>
2015-10-16 20:25:00 -0300

    pruebas unitarias usuario, proyecto

--- proyecto/tests.py
+++ proyecto/tests.py
@@ -1,3 +1,89 @@
+import datetime
 from django.test import TestCase
+from django.test.client import RequestFactory
+from django.contrib.auth.models import User
+from proyecto.models import Proyecto
 
-# Create your tests here.
+
+
+class ProyectosTest(TestCase):
+    """
+    Clase que realiza el Test del modulo de administracion de proyectos
+    """
+    def setUp(self):
+        """
+        Funcion que inicializa el RequestFactory y un usuario de prueba para
+        realizar los test
+        """
+        # Se crea el Request factory pars simular peticiones
+        self.factory = RequestFactory()
+        # Se crea el User que realiza las peticiones
+        self.user = User.objects.create_user(username='testuser', email='test@test.com', password='test')
+
+    def test_view_nuevo_proyecto(self):
+        """
+        Funcion que realiza el test sobre la vista ProyectoCreate que crea
+        un nuevo usuario
+        """
+        # se loguea el usuario testuser
+        user = self.client.login(username='testuser', password='test')
+        self.assertTrue(user)
+
+        user3 = User.objects.create_user(username='user_prueba3', email='test@test223.com', password='prueba')
+        # se crea un usuario
+        proyecto = Proyecto.objects.create(codigo='codi', nombre_corto='test',
+                                           nombre_largo='test', cancelado=False, scrum_master=user3)
+
+        self.assertEqual(proyecto.codigo, 'codi')
+        self.assertEqual(proyecto.nombre_corto, 'test')
+
+        print 'Test de nuevo_proyecto realizado exitosamente'
+
+    def test_view_editar_proyecto(self):
+        """
+        Funcion que realiza el test sobre la vista ProyectoUpdate que modifica
+        un proyectos existente
+        """
+        # se loguea el usuario testuser
+        user = self.client.login(username='testuser', password='test')
+        self.assertTrue(user)
+
+        user4 = User.objects.create_user(username='user_prueba4', email='test@test224.com', password='prueba')
+        # se crea un usuario
+        proyecto = Proyecto.objects.create(codigo='codi', nombre_corto='test',
+                                           nombre_largo='test', cancelado=False, scrum_master=user4)
+
+        # se crean nuevos valores para los atributos
+        nuevo_codigo = 'new'
+        new_nombre = 'Hola'
+
+        # Se modifican los atributos del usuario
+        proyecto.codigo = nuevo_codigo
+        proyecto.nombre_corto = new_nombre
+        proyecto.save()
+
+        self.assertEqual(proyecto.codigo, 'new')
+        self.assertEqual(proyecto.nombre_corto, 'Hola')
+
+        print 'Test de editar_proyecto realizado exitosamente'
+
+    def test_view_desactivar_proyecto(self):
+        """
+        Funcion que realiza el test sobre la vista proyecto que cambia el estado
+        de un proyecto a cancelado
+        """
+        # se loguea el usuario testuser
+        user = self.client.login(username='testuser', password='test')
+        self.assertTrue(user)
+
+        user5 = User.objects.create_user(username='user_prueba5', email='test@test225.com', password='prueba')
+        # se crea un usuario
+        proyecto = Proyecto.objects.create(codigo='codi', nombre_corto='test',
+                                           nombre_largo='test', cancelado=False, scrum_master=user5)
+        # se marca al proyecto
+        proyecto.cancelado = True
+        proyecto.save()
+
+        self.assertEqual(proyecto.cancelado, True)
+
+        print 'Test de desactivar_Proyecto realizado exitosamente'
--- usuario/tests.py
+++ usuario/tests.py
@@ -1,3 +1,85 @@
+from django.test.client import RequestFactory
 from django.test import TestCase
+from django.contrib.auth.models import User
+from usuario.views import usuarios, nuevo_usuario
+from models import Usuario
 
-# Create your tests here.
+class UsuariosTest(TestCase):
+    """
+    Clase que realiza el Test del modulo de administracion de usuarios
+    """
+    def setUp(self):
+        """
+        Funcion que inicializa el RequestFactory y un usuario de prueba para
+        realizar los test
+        """
+        # Se crea el Request factory pars simular peticiones
+        self.factory = RequestFactory()
+        # Se crea el User que realiza las peticiones
+        self.user = User.objects.create_user(username='testuser', email='test@test.com', password='test')
+
+
+
+    def test_view_nuevo_usuario(self):
+        """
+        Funcion que realiza el test sobre la vista UserCreate que crea
+        un nuevo usuario
+        """
+        # se loguea el usuario testuser
+
+
+        # se crea un usuario
+        user = User.objects.create_user(username='user_prueba', email='test@test2.com', password='prueba')
+        Usuario.objects.create(user=user, telefono='222', direccion='Avenida', documento='12345')
+
+        self.assertEqual(Usuario.objects.get(user=user).user.username, 'user_prueba')
+        self.assertEqual(Usuario.objects.get(user=user).user.email, 'test@test2.com')
+        self.assertEqual(Usuario.objects.get(user=user).telefono, '222')
+        self.assertEqual(Usuario.objects.get(user=user).documento, '12345')
+
+
+        print 'Test de nuevo_usuario realizado exitosamente'
+
+
+    def test_view_desactivar_usuario(self):
+        """
+        Funcion que realiza el test sobre la vista inactivar_usuario que cambia el estado
+        de un usuario a inactivo
+        """
+        # se loguea el usuario testuser
+        user = self.client.login(username='testuser', password='test')
+        self.assertTrue(user)
+        # se crea un usuario
+        user = User.objects.create_user(username='user_prueba', email='test@test4.com', password='prueba')
+        usuario_prueba = Usuario.objects.create(user=user, telefono='222', direccion='Avenida', documento='12345')
+        # se marca al usuario como inactivo
+        usuario_prueba.user.is_active = False
+        usuario_prueba.save()
+
+        self.assertEqual(usuario_prueba.user.is_active, False)
+
+        print 'Test de desactivar_usuario realizado exitosamente'
+
+    def test_view_activar_usuario(self):
+        """
+        Funcion que realiza el test sobre la vista activar_usuario que cambia el estado
+        de un usuario a activo
+        """
+        # se loguea el usuario testuser
+        user = self.client.login(username='testuser', password='test')
+        self.assertTrue(user)
+        # se crea un usuario
+        user = User.objects.create_user(username='user_prueba', email='test@test5.com', password='prueba')
+        usuario_prueba = Usuario.objects.create(user=user, telefono='222', direccion='Avenida', documento='12345')
+        # se marca al usuario como inactivo
+        usuario_prueba.user.is_active = False
+        usuario_prueba.save()
+        self.assertEqual(usuario_prueba.user.is_active, False)
+        # se marca al usuario como activo
+        usuario_prueba.user.is_active = True
+        usuario_prueba.save()
+
+        self.assertEqual(usuario_prueba.user.is_active, True)
+
+        print 'Test de activar_usuario realizado exitosamente'
+
