2745dbdcbe508aa6f6f8bf8e753c0171c90563bd
Jose Segovia <josesego@hotmail.es>
2015-10-18 15:01:40 -0300

    Ya no se piden actividades al crear Flujo

--- flujos/forms.py
+++ flujos/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.forms import ModelMultipleChoiceField
-from models import Flujo, Actividad,Estado
+from models import Flujo, Actividad, Estado
 from proyecto.models import Proyecto
 
 
@@ -10,16 +10,15 @@ class MyModelMultipleChoiceField(ModelMultipleChoiceField):
 
 
 class FlujoCreateForm(forms.ModelForm):
-    actividades = ModelMultipleChoiceField(Actividad.objects.all(),
-            widget=forms.CheckboxSelectMultiple, required=True)
+    # actividades = ModelMultipleChoiceField(Actividad.objects.all(),
+    #                                        widget=forms.CheckboxSelectMultiple, required=True)
+
     class Meta:
         model = Flujo
-        fields = [ 'nombre', 'actividades','descripcion','proyecto']
-
+        fields = ['nombre', 'descripcion', 'proyecto']
 
 
 class ActividadCreateForm(forms.ModelForm):
-
     class Meta:
         model = Actividad
         fields = ['nombre']
@@ -45,43 +44,40 @@ class ActividadCreateForm(forms.ModelForm):
         return actividad
 
 
-class AsignarFlujoProyectoForm(forms.ModelForm):
-    def __init__(self, user, *args, **kwargs):
-        context = super(AsignarFlujoProyectoForm, self).__init__(*args, **kwargs)
-        proyecto = Proyecto.objects.get(pk=kwargs['instance'].pk)
-        flujos_actuales = Flujo.objects.filter(proyecto=proyecto)
-
-
-        self.fields['flujos'] = forms.ModelMultipleChoiceField(Flujo.objects.all().order_by('id'),
-                                                               widget=forms.CheckboxSelectMultiple, required=True)
-
-        dic = {}
-        for arr in flujos_actuales:
-            dic[arr.pk] = arr
-        self.fields['flujos'].initial = dic
-
-
-    codigo = forms.CharField(widget=forms.HiddenInput(), required=True)
-
-
-    class Meta:
-        model = Proyecto
-        fields = ['codigo']
-
-    def save(self, commit=True):
-        if not commit:
-            raise NotImplementedError("Can't create Flujo without database save")
-
-        proyecto = super(AsignarFlujoProyectoForm, self).save(commit=True)
-
-        lista_completa = Flujo.objects.all()
-        print "self %s" % self.cleaned_data['flujos']
-        for flujo in self.cleaned_data['flujos']:
-            flujo.proyecto = proyecto
-            flujo.save()
-            for f in lista_completa:
-                if f not in self.cleaned_data['flujos']:
-                    f.proyecto = None
-                    f.save()
-
-        return proyecto
+# class AsignarFlujoProyectoForm(forms.ModelForm):
+#     def __init__(self, user, *args, **kwargs):
+#         context = super(AsignarFlujoProyectoForm, self).__init__(*args, **kwargs)
+#         proyecto = Proyecto.objects.get(pk=kwargs['instance'].pk)
+#         flujos_actuales = Flujo.objects.filter(proyecto=proyecto)
+#
+#         self.fields['flujos'] = forms.ModelMultipleChoiceField(Flujo.objects.all().order_by('id'),
+#                                                                widget=forms.CheckboxSelectMultiple, required=True)
+#
+#         dic = {}
+#         for arr in flujos_actuales:
+#             dic[arr.pk] = arr
+#         self.fields['flujos'].initial = dic
+#
+#     codigo = forms.CharField(widget=forms.HiddenInput(), required=True)
+#
+#     class Meta:
+#         model = Proyecto
+#         fields = ['codigo']
+#
+#     def save(self, commit=True):
+#         if not commit:
+#             raise NotImplementedError("Can't create Flujo without database save")
+#
+#         proyecto = super(AsignarFlujoProyectoForm, self).save(commit=True)
+#
+#         lista_completa = Flujo.objects.all()
+#         print "self %s" % self.cleaned_data['flujos']
+#         for flujo in self.cleaned_data['flujos']:
+#             flujo.proyecto = proyecto
+#             flujo.save()
+#             for f in lista_completa:
+#                 if f not in self.cleaned_data['flujos']:
+#                     f.proyecto = None
+#                     f.save()
+#
+#         return proyecto
