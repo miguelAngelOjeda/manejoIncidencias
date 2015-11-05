f01a2aeb5c807028aac49e9bf7c025377aca1967
Jose Segovia <josesego@hotmail.es>
2015-11-04 21:35:15 -0300

    Arreglo menor

--- flujos/views.py
+++ flujos/views.py
@@ -7,20 +7,26 @@ from django.template import RequestContext
 from proyecto.models import Proyecto
 from forms import ActividadCreateForm, FlujoCreateForm
 # from form import AsignarFlujoProyectoForm
-from models import Flujo, Actividad,Estado
+from models import Flujo, Actividad, Estado
 
 # Create your views here.
+from user_story.models import UserStory
+
 
 def flujos(request):
     usuario = request.user
     flujos = Flujo.objects.all()
-    return render_to_response('flujos.html',{'flujos':flujos,'usuario':usuario}, context_instance=RequestContext(request))
+    return render_to_response('flujos.html', {'flujos': flujos, 'usuario': usuario},
+                              context_instance=RequestContext(request))
 
 
 def actividades(request):
     usuario = request.user
     actividades = Actividad.objects.all()
-    return render_to_response('actividades.html',{'actividades':actividades,'usuario':usuario}, context_instance=RequestContext(request))
+    user_story = UserStory.objects.all()
+    return render_to_response('actividades.html',
+                              {'actividades': actividades, 'usuario': usuario, 'user_story': user_story},
+                              context_instance=RequestContext(request))
 
 
 def nueva_actividad(request):
@@ -38,10 +44,13 @@ def nueva_actividad(request):
                 return HttpResponseRedirect('/../actividades')
             except:
                 error = 'Error al procesar la entidad'
-                return render_to_response('crear_actividad.html',{'formulario':formulario,'errors':error,'usuario':usuario}, context_instance=RequestContext(request))
+                return render_to_response('crear_actividad.html',
+                                          {'formulario': formulario, 'errors': error, 'usuario': usuario},
+                                          context_instance=RequestContext(request))
     else:
         formulario = ActividadCreateForm()
-    return render_to_response('crear_actividad.html', {'formulario': formulario,'usuario':usuario}, context_instance=RequestContext(request))
+    return render_to_response('crear_actividad.html', {'formulario': formulario, 'usuario': usuario},
+                              context_instance=RequestContext(request))
 
 
 def nuevo_flujo(request):
@@ -59,10 +68,14 @@ def nuevo_flujo(request):
                 return HttpResponseRedirect('/../flujos')
             except:
                 error = 'Error al procesar la entidad'
-                return render_to_response('crear_flujo.html',{'formulario':formulario,'errors':error,'usuario':usuario}, context_instance=RequestContext(request))
+                return render_to_response('crear_flujo.html',
+                                          {'formulario': formulario, 'errors': error, 'usuario': usuario},
+                                          context_instance=RequestContext(request))
     else:
         formulario = FlujoCreateForm()
     return render_to_response('crear_flujo.html', {'formulario': formulario}, context_instance=RequestContext(request))
+
+
 def desactivar(request, pk_flujo):
     """
     Funcion que inactiva la cuenta de un flujo seleccionado.
@@ -81,10 +94,12 @@ def desactivar(request, pk_flujo):
     user_detail = get_object_or_404(Actividad, pk=pk_flujo)
     user_detail.is_active = False
     user_detail.save()
-    mensaje ="El usuario se desactivo con exito."
+    mensaje = "El usuario se desactivo con exito."
 
     usuario = Actividad.objects.all()
-    return render_to_response('usuarios.html', {'usuarios': usuario,'mensajes':mensaje,'usuario':usuariolB}, context_instance=RequestContext(request))
+    return render_to_response('usuarios.html', {'usuarios': usuario, 'mensajes': mensaje, 'usuario': usuariolB},
+                              context_instance=RequestContext(request))
+
 
 def activar(request, pk_flujo):
     """
@@ -105,8 +120,8 @@ def activar(request, pk_flujo):
     user_detail.is_active = True
     user_detail.save()
 
-    mensaje ="El usuario se activo con exito."
+    mensaje = "El usuario se activo con exito."
 
     usuario = Actividad.objects.all()
-    return render_to_response('usuarios.html', {'usuarios': usuario,'mensajes':mensaje,'usuario':usuariolB}, context_instance=RequestContext(request))
-
+    return render_to_response('usuarios.html', {'usuarios': usuario, 'mensajes': mensaje, 'usuario': usuariolB},
+                              context_instance=RequestContext(request))
